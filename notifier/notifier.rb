fe643ef5e4c464aa728ad805743e6069fb8ad286
Jimmy@cp.ubu <jimmy@coolpur.com>
2010-01-25 14:26:27 +0800

    a

--- notifier/notifier.rb
+++ notifier/notifier.rb
@@ -1,48 +1,38 @@
+#!/usr/bin/env ruby
+
 require 'db_model'
 require 'sms'
 require 'email'
 
 class ProcessNotifier 
 
-  def self.process()
-	  sms = Sms.new
-	  em  = Email.new
-	  loop do
-	  log ' ==> ' << Time.now.to_s
-	  ns = Notifier.all(:conditions => "status = 0") 
-	  if ns && ns.length>0
-		  ns.each do |n|
-			  if (n.ntype == "SMS")
-				  r = sms.SendSms(n.address, n.message)
-				  if r==0
-					  log "Send sms(#{n.id}) to: #{n.address}, message: #{n.message}"
-					  n.sent_date = Time.now
-					  n.status = 1
-					  n.save
-				  else
-					  log "Send sms(#{n.id}) error"
-				  end
-			  end
-			  if (n.ntype == "EMAIL")
-				  log "Send email(#{n.id}): #{n.address},  subject: #{n.subject}"
-				  em.SendEmail(n.address, n.subject, n.message)
-				  n.sent_date = Time.now
-				  n.status = 1
-				  n.save
-			  end
-		  end
-		  log ' ---e---- \n'
-	  end
-
-	  sleep(10)
-	  end
-  end
-
-  def self.log(message)
-	  File.open('notifier.out','a') do |f|
-		  f.puts message
-	  end
-  end
+	def self.process()
+		sms = Sms.new
+		em  = Email.new
+		File.open('notifier.out','a') do |f|
+			f.puts ' ==> ' << Time.now.to_s
+			Notifier.all(:conditions => "status = 0").each do |n|
+				if (n.ntype == "SMS")
+					r = sms.SendSms(n.address, n.message)
+					if r==0
+						f.puts "Send sms(#{n.id}) to: #{n.address}, message: #{n.message}"
+						n.sent_date = Time.now
+						n.status = 1
+						n.save
+					else
+						f.puts "Send sms(#{n.id}) error"
+					end
+				end
+				if (n.ntype == "EMAIL")
+					f.puts "Send email(#{n.id}): #{n.address},  subject: #{n.subject}"
+					em.SendEmail(n.address, n.subject, n.message)
+					n.sent_date = Time.now
+					n.status = 1
+					n.save
+				end
+			end
+		end
+	end
 end
 
 ProcessNotifier.process
--- notifier/sms.rb
+++ notifier/sms.rb
@@ -3,10 +3,8 @@ require 'soap/wsdlDriver'
 
 class Sms
   def initialize
-    #Open the YAML configuration file
     @config = YAML.load_file("../config/config.yml")["development"]
     @service = SOAP::WSDLDriverFactory.new(@config["sms_wsdl"]).create_rpc_driver
-    # Log SOAP request and response
     # @service.wiredump_file_base = "soap-log.txt"
   end
   
